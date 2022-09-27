ca904d6947f7e0b12dac131b59ce095c628f2b89
Agustin Leguizamon <agustin.leg7@gmail.com>
2022-09-26 21:55:06 -0300

    Lunes 26 sep Clase-01

---
+++ Clase-01/main.js
@@ -0,0 +1,121 @@
+// Variables
+// Es case sensitive
+// No es tipado
+
+// ES5 => ES = EcmaScript
+/*
+ var nombre = "Gabo";
+ var edad = 28;
+*/
+
+// ES6
+
+// Strings
+let nombre = "Agustin";
+let saludo = 'El nombre es "Agustin"';
+let mensaje = "hola mundo";
+
+// Numbers
+
+let edad = 22;
+let negativo = -2;
+let conComa = 2.2;
+
+// Boleans
+// camelCase
+const esMayorDeEdad = false;
+
+// PascalCase
+const EsMayorDeEdad = true;
+
+//  UPPER_SNAKE_CASE
+const ES_MAYOR_DE_EDAD = true;
+
+// Snake_Case
+const es_mayor_de_edad = true;
+
+// Nulos
+let sinDefinir; // = undefined
+let sinValor = null;
+
+// Infinity Y NaN
+let infinito = Infinity;
+// let infinito = -Infinity;
+let noEsUnNumero = NaN;
+
+nombre = "Gabo";
+// no se puede cambiar el valor de una constante
+// edad = 28;
+// console.log(dni);
+// console.log(nombre);
+// console.log(edad);
+// console.log(saludo);
+// console.log(mensaje);
+// console.log(EsMayorDeEdad);
+// console.log(sinDefinir);
+// console.log(sinValor);
+
+// Operadores Aritmeticos
+// Suma
+// console.log("La suma de 2 + 2 =" + (2 + 2));
+
+// Resta
+// console.log("La resta de 10 -2 =", 10 - 2);
+
+// Multiplicacion
+// console.log(2 * 2);
+
+// Division
+// console.log(212312 / 68923.23);
+
+// Modulo - Restante
+// console.log(10 % 2);
+
+// let saludoDos = "BIENVENIDO " + nombre + "Como estas?" + 412241;
+let saludoDos = `BIENVENIDO ${nombre} Como estas? ${2 + 2}`;
+
+// console.log(saludoDos);
+
+const dni = 1123124512;
+// console.log(typeof nombre);
+
+let numeroUno = 10;
+let numeroDos = "30";
+let numeroTres = "hola";
+
+// console.log(typeof numeroDos);
+// console.log(typeof Number(numeroDos));
+
+// NaN
+// console.log(Number(numeroTres) + 1);
+
+// sumar numero + un booleano
+const prueba = false;
+// console.log(10 + prueba);
+// console.log(Number(prueba));
+
+// console.log(Boolean(""));
+
+// console.log(numeroUno + Number(numeroDos));
+
+const bienvenida = `Nombre: ${nombre} - DNI: ${dni} - Edad: ${edad}`;
+
+// console.log(bienvenida);
+// Interactuar con el index.html (documento)
+// document.write(bienvenida);
+
+// Crear multiples variables
+let variable1 = 1,
+  varibale2 = 2,
+  variablesTres = 3; // NO nombrar asi las variables ❌
+
+// una constante no puede inicializar sin dato => undefined
+// const constanteUndefined;
+
+// console.log(typeof variable1);
+// alert("hola");
+
+// let saludoPrompt = prompt("hola");
+// console.log(Number(saludoPrompt));
+
+// confirm("Tenes mas de 18 años?");
